36e0c2c4732a77566f5552d1e8de1a88030fc6a9
MindaugasPu <mindaugas.pupienis@gmail.com>
2023-02-07 14:08:24 +0200

    išsaugau į master branch dalį darbo

--- TODOlist/mysite/todo/urls.py
+++ TODOlist/mysite/todo/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('manouzduotys/', views.UzduotysListView.as_view(), name='manouzduotys'),
     path('manouzduotys/nauja/', views.UserUzduotisCreateView.as_view(), name='nauja'),
+    path('manouzduotys/<int:pk>/update', views.UzduotisUpdateView.as_view(), name='redaguoti'),
+    path('manouzduotys/<int:pk>/delete', views.UzduotisDeleteView.as_view(), name='istrinti'),
 ]
--- TODOlist/mysite/todo/views.py
+++ TODOlist/mysite/todo/views.py
@@ -11,6 +11,7 @@ def index(request):
 class UzduotysListView(LoginRequiredMixin, generic.ListView):
     model = Uzduotis
     template_name = 'manouzduotys.html'
+    paginate_by = 4
 
     def get_queryset(self):
         return Uzduotis.objects.filter(vartotojas=self.request.user)
@@ -25,3 +26,25 @@ class UserUzduotisCreateView(LoginRequiredMixin, generic.CreateView):
     def form_valid(self, form):
         form.instance.vartotojas = self.request.user
         return super().form_valid(form)
+
+
+class UzduotisUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Uzduotis
+    fields = ['uzduotis']
+    success_url = '/todo/manouzduotys'
+    template_name = 'user_uzduotis_form.html'
+
+    def form_valid(self, form):
+        form.instance.vartotojas = self.request.user
+        return super().form_valid(form)
+
+
+class UzduotisDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Uzduotis
+    success_url = '/todo/manouzduotys'
+    template_name = 'trinimas_form.html'
+
+    def form_valid(self, form):
+        form.vartotojas = self.request.user
+        return super().form_valid(form)
+
